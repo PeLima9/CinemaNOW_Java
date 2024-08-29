6ab410b58d60fce24ae60fc634165d7c92b657f7
PeLima9 <eduardolima102a15@gmail.com>
2024-08-29 15:11:46 -0600

    Se me olvidó cambiar de rama [Clase Usuario]

---
+++ src/Modelo/Usuario.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author eduar
+ */
+public class Usuario {
+    private String UUID_Usuario;
+    private String Nombre;
+    private String Email;
+    private String Password;
+    private int Rol;
+    private String FotoPerfil;
+    
+    public String getUUID_Usuario() {
+        return UUID_Usuario;
+    }
+
+    public void setUUID_Usuario(String UUID_Usuario) {
+        this.UUID_Usuario = UUID_Usuario;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public int getRol() {
+        return Rol;
+    }
+
+    public void setRol(int Rol) {
+        this.Rol = Rol;
+    }
+
+    public String getFotoPerfil() {
+        return FotoPerfil;
+    }
+
+    public void setFotoPerfil(String FotoPerfil) {
+        this.FotoPerfil = FotoPerfil;
+    }
+    
+    
+}
