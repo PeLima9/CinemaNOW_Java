22dc39ebb4e03f40349c584295c0cac52292fa39
stanlyy <20220127@ricaldone.edu.sv>
2024-08-30 12:12:39 -0600

    PAPI MIRANDA

---
+++ src/Modelo/Cartelera.java
@@ -0,0 +1,18 @@
+package Modelo;
+
+import java.sql.*;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import Vista.FrmCartelera;
+
+public class Cartelera {
+                          
+    private String pelicula_id;
+    private String titulo;
+    private String email;
+    private String contraseña;
+    private int rol_id;
+    private String foto_perfil;
+
+                                                
+}
