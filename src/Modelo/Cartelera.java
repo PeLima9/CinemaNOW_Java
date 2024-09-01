27da4ecc2e50c9cddcae949b7afe4bb3be8491c1
Nightmare <lfernandonaleman@gmail.com>
2024-09-01 17:42:03 -0600

    Bla bla

---
+++ src/Modelo/Cartelera.java
@@ -0,0 +1,20 @@
+
+package Modelo;
+
+import java.sql.*;
+import java.util.UUID;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import Vista.FrmCartelera;
+
+public class Cartelera {
+     private int pelicula_id;
+     private String titulo;
+    private String descripcion;
+     private int duracion;
+    private String clasficacion;
+    private String genero;
+    private String poster;
+   
+    private String foto_perfil;
+}
