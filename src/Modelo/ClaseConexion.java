e3211789877b37915694c7e4bcd1e80010b71f55
Maxi <chanakane820@gmail.com>
2024-10-17 11:24:29 -0600

    primera version post expo

--- src/Modelo/ClaseConexion.java
+++ src/Modelo/ClaseConexion.java
@@ -6,7 +6,7 @@ import java.sql.*;
 public class ClaseConexion {
      //Variables para la cadena de conexión
     private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
-    private static final String USUARIO = "CinemaNOW";
+    private static final String USUARIO = "EXPO";
     private static final String CONTRASENA = "Estrella_Bella!";
     //Creación del metodo de conexión que retorna la conexión
     public static Connection getConexion() {
